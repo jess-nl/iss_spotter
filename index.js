5e5ad1f07b8b12a9017987a9e028adaa7fcdf0c0
Jess <jnoriegaless@gmail.com>
2019-07-05 02:52:43 +0000

    iss spotter 4

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-});
 
-fetchCoordsByIP('8.8.8.8', (error, coord) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('8.8.8.8', (error, coord) => {
+//   if (error) {
+//     console.log("It didn't work!"   , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , coord);
+// });
+
+// fetchISSFlyOverTimes({ lat: '37.38600', long: '-122.08380' }, (error, coord) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , coord);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
-  console.log('It worked! Returned IP:' , coord);
+};
+
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
--- iss.js
+++ iss.js
@@ -25,14 +25,59 @@ const fetchCoordsByIP = (ip, callback) => {
       callback(`Error 💀, the response status is at ${response.statusCode} on IP ${body}`, null);
     } else {
       const coordinates = JSON.parse(body);
-      callback(null, coordinates.data.latitude);
-      callback(null, coordinates.data.longitude);
+      const lat = coordinates.data.latitude;
+      const long = coordinates.data.longitude;
+      callback(null, {lat, long});
+      // callback(null, coordinates.data.altitude;
+      // console.log(body)
     }
   });
 
 };
 
+const fetchISSFlyOverTimes = function(coords, callback) {
+
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.lat}&lon=${coords.long}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+      callback(`Error 💀, the response status is at ${response.statusCode} on IP ${body}`, null);
+    } else {
+      const coordinates = JSON.parse(body);
+      callback(null, coordinates.response);
+    }
+  });
+
+};
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+      console.log("test");
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+console.log("test");
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+
 module.exports = {
-  fetchMyIP, 
-  fetchCoordsByIP
+  fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
 };
